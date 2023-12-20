c1fab707e91992a8856faee90d9955da883896e2
Simone Ricci <simogol@gmail.com>
2023-12-20 18:10:15 +0100

    Update index.php

--- php/src/index.php
+++ php/src/index.php
@@ -1,6 +1,6 @@
 <?php
 // index.php
-// Note: $host value should be the same defined in docker-compose.yml as services.db.container_name value.
+// Note: $host value should be the same define as services.db.container_name in docker-compose.yml file
 $host = 'db';
 $user = 'myuser';
 $pass = 'mypass';
